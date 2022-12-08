b59cf14e778b897c173be1e11cdb08fb021fe834
Leo Drieskens <ldrieske@fu-r3-p7.s19.be>
2022-12-08 19:20:23 +0100

    need to find a solution for the \n problem

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ldrieske <ldrieske@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 16:07:57 by ldrieske          #+#    #+#             */
-/*   Updated: 2022/12/08 15:54:11 by ldrieske         ###   ########.fr       */
+/*   Updated: 2022/12/08 19:18:07 by ldrieske         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,82 +15,63 @@
 #include <unistd.h>
 
 int	ft_putnbr_base(unsigned int nb, char *base, int len);
+int	ft_putchar(int c);
 
-static void	ft_putstr(const char *str)
+static int	ft_putstr(const char *str)
 {
 	int	i;
 
 	i = 0;
-	while (str[i] != '\0')
+	while (str[i])
 		write(1, &str[i++], 1);
+	return (i);
 }
 
-static void	ft_percentchar(char a, va_list b)
+static int	ft_percentchar(char a, va_list b)
 {
+	int	c;
+
+	c = 0;
 	if (a == 'c')
-	{
-		int	c;
-		c = va_arg(b, int);
-		write(1, &c, 1);
-	}
+		return (ft_putchar(va_arg(b, int)));
+	if (a == 's')
+		return (ft_putstr(va_arg(b, char *)));
 	if (a == 'd')
 	{
-		int	c;
 		c = va_arg(b, int);
 		if (c < 0)
-		{
-			c = -c;
-			write(1, "-", 1);
-		}
-		ft_putnbr_base(c, "0123456789", 10);
+			ft_putchar(-c);
+		return (ft_putnbr_base(c, "0123456789", 10));
 	}
-	if (a == 'o')
-	{
-		int	c;
-		c = va_arg(b, int);
-		ft_putnbr_base((int)c, "01234567", 8);
-	}
-	if (a == 's')
-	{	
-		char	*c;
-		c = va_arg(b, char *);
-		ft_putstr(c);
-	}
-	if (a == 'x') 
-	{	
-		int c;
-		c = va_arg(b, int);
-		ft_putnbr_base(c, "0123456789abcdef", 16);
-	}
+	if (a == 'x')
+		return (ft_putnbr_base(va_arg(b, int), "0123456789abcdef", 16));
 	if (a == 'X')
-	{	
-		int c;
-		c = va_arg(b, int);
-		ft_putnbr_base(c, "0123456789ABCDEF", 16);
-	}
+		return (ft_putnbr_base(va_arg(b, int), "0123456789ABCDEF", 16));
 	if (a == '%')
 		write(1, "%", 1);
+	return (0);
 }
 
 int	ft_printf(const char *format, ...)
 {
 	unsigned int	j;
-	size_t			i;
+	int				i;
 	va_list			arg;
 
 	j = 0;
 	i = 0;
 	va_start(arg, format);
-	while (format[j] != '\0')
+	while (format[j])
 	{
 		while (format[j] != '%' && format[j] != '\0')
 		{
 			write(1, &format[j], 1);
 			j++;
 		}
-		if (format[j++] == '%')
-			ft_percentchar(format[j], arg);
-		j++;
+		if (format[j++] == '%' && format[j] != '\0')
+			i += ft_percentchar(format[j], arg);
+		if (format[j])
+			j++;
 	}
 	va_end(arg);
 	return (i);
@@ -99,13 +80,13 @@ int	ft_printf(const char *format, ...)
 int	main(void)
 {
 	// printf("<PRINTF> yo les amis %d %s %o %x %c %X %%\n", 9, "coucou", 10, 254, 'a', 254);
-	ft_printf("c : %c\nd : %d\no : %o\ns : %s\nx : %x\nX : %X\npourcent : %%\n", 'a', -42, 12, "coucou", 254, 254);
-	// myprintf("d : %d\n", 9999);
-	// myprintf("s : %s\n", "Salut");
-	// myprintf("o : %o\n", 10);
-	// myprintf("x : %x\n", 254);
-	// myprintf("X : %X\n", 254);
-	// myprintf("pourcent : %%\n");
+	// ft_printf("c : %c\nd : %d\ns : %s\nx : %x\nX : %X\npourcent : %%\n", 'a', -42, "coucouuuuu", 254, 254);
+	ft_printf("c : %c", 'a');
+	ft_printf("d : %d", 9999);
+	ft_printf("s : %s", "i");
+	ft_printf("x : %x", 254);
+	ft_printf("X : %X", 254);
+	ft_printf("pourcent : %%");
 	// printf("c : %c\n", 'a');
 	// printf("d : %d\n", -9999);
 	return (0);
--- ft_printf/ft_putnbr_base.c
+++ ft_printf/ft_putnbr_base.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbrbase.c                                    :+:      :+:    :+:   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ldrieske <ldrieske@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 14:41:50 by ldrieske          #+#    #+#             */
-/*   Updated: 2022/12/08 14:41:51 by ldrieske         ###   ########.fr       */
+/*   Updated: 2022/12/08 18:48:13 by ldrieske         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,17 @@
 
 int	ft_putnbr_base(unsigned int nb, char *base, int len)
 {
-	int count;
+	int	count;
 
 	count = 0;
 	if (nb < (unsigned int)len)
 		return (write(1, &base[nb], 1));
 	count += ft_putnbr_base(nb / len, base, len);
 	return (count + ft_putnbr_base(nb % len, base, len));
+}
+
+int	ft_putchar(int c)
+{
+	write(1, &c, 1);
+	return (1);
 }
