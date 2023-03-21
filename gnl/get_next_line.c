9172af7c24d4881da86f21fa095c67324e4b80a3
Leo Drieskens <ldrieske@mi-r5-p3.s19.be>
2023-03-21 11:51:00 +0100

    test01 ssh GNL

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ldrieske <ldrieske@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 20:55:27 by ldrieske          #+#    #+#             */
-/*   Updated: 2023/03/21 11:12:47 by ldrieske         ###   ########.fr       */
+/*   Updated: 2023/03/21 11:45:07 by ldrieske         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	line_count(char line)
 {
-	static int count;
+	static int	count;
 	(void)line;
 
 	count = 0;
@@ -33,33 +33,32 @@ char	*get_next_line(int fd)
 {
 	int		size;
 	char	*buffer;
-	size_t	numBytesRead;
+	size_t	numbytes;
 
 	size = (int)BUFFER_SIZE;
 	if (size > 100000 || size < 1)
 		size = 2048;
-
 	buffer = malloc(sizeof(char) * (size + 1));
 	if (!buffer)
 		return (0);
-
-	numBytesRead = 1;
-	while (numBytesRead > 0) {
-		numBytesRead = read(fd, buffer, BUFFER_SIZE);
-		write(1, buffer, numBytesRead);
+	numbytes = 1;
+	while (numbytes > 0)
+	{
+		numbytes = read(fd, buffer, BUFFER_SIZE);
+		write(1, buffer, numbytes);
 	}
-	buffer[numBytesRead] = 0;
+	buffer[numbytes] = 0;
 	return (0);
 }
 
-int main()
+int	main(void)
 {
 	int		fd;
+
 	fd = open("del_texte", O_RDONLY);
 	if (fd == -1)
 		return (0);
-
 	get_next_line(fd);
-    close(fd);
+	close(fd);
 	return (0);
 }
