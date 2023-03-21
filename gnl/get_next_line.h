a6d3e5a03432885a56e89b0cfa3169a9788be346
Leo Drieskens <ldrieske@mi-r5-p3.s19.be>
2023-03-21 11:35:28 +0100

    test00

--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -6,15 +6,17 @@
 /*   By: ldrieske <ldrieske@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 20:53:30 by ldrieske          #+#    #+#             */
-/*   Updated: 2023/03/19 19:02:05 by ldrieske         ###   ########.fr       */
+/*   Updated: 2023/03/21 11:08:45 by ldrieske         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <stdio.h>
+# include <stdlib.h>
 # include <unistd.h>
-# include <stdarg.h>
+# include <fcntl.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 80000
